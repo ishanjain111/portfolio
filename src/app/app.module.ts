843b79d59a901cd06b22ba67558491d2f1c7806a
Ishan Jain <ishan.jain@biz2credit.com>
2023-03-05 00:19:05 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { KnowMoreComponent } from './know-more/know-more.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ConnectComponent } from './connect/connect.component';
+import { FormsModule } from '@angular/forms';
+// import { AppRoutingModule } from './app-routing'
+
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "Home", component: HomeComponent},
+  {path: "know",component: KnowMoreComponent},
+  {path: "connect", component: ConnectComponent}
+
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    KnowMoreComponent,
+    ConnectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/know-more/know-more.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-know-more',
+  templateUrl: './know-more.component.html',
+  styleUrls: ['./know-more.component.css']
+})
+export class KnowMoreComponent implements OnInit {
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+  }
+  downloadFile(){
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target','_blank');
+    link.setAttribute('href','assets/9919103130 IshanJain.pdf')
+    link.setAttribute('download','resume.pdf');
+    link.click();
+    link.remove();
+  }
+
+  academics = [
+    {year: "2019 - 2023", degree:"B.tech in Computer Science and Engineering",institute:"Jaypee institute Of Information Technology", score:"CGPA: 7.7"},
+    {year: "2016 - 2018", degree:"AISSCE", institute:"Meerut Public School, Meerut", score:"Percentage: 95.2%"},
+    {year: "2014 - 2016", degree:"AISSE",institute:"Meerut Public School, Meerut", score:"CGPA: 10"}
+  ]
+
+  projects = [
+    {title: "Analysis Of Chest X-Ray", description:"Built a CNN-based model to predict wether the patient is suffering from either COVID or Pneumonia. In Input layers we have used relu and in output layer softmax as an activation function. Categorical crossentropy is used as a loss function."},
+    {title:"Movie Recommendation",description:"A content-based movie Recommendation model made with python and C++ language which suggest movies according to your past behavior. The application uses cosine-similarity to find similiarities between different movies in the dataset with previous searches."},
+    {title: "Weather Application", description:"This a weather application where user can know the real-time weather condition of the desired city along with that the latitude and longitude value of the city. This application is built using HTML, CSS, JS, ExpressJs and OpenWeather Api. Docker is used for containerization."}
+  ]
+
+}
